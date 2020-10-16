8d1ba776b0d654d3284c0bb505bcf42d64a4a323
joseuam <63088040+joseuam@users.noreply.github.com>
2020-10-16 11:15:09 -0500

    ..Drag

--- HackatonFront/src/app/app.module.ts
+++ HackatonFront/src/app/app.module.ts
@@ -8,11 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+	  BrowserModule, 
+	  IonicModule.forRoot(), 
+	  AppRoutingModule, 
+	  BrowserAnimationsModule,
+	  DragDropModule
+  ],
+
   providers: [
     StatusBar,
     SplashScreen,
