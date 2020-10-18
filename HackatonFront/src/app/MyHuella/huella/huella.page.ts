af834b54895866974afb97826d385905a9b4b833
joseuam <63088040+joseuam@users.noreply.github.com>
2020-10-18 03:17:33 -0500

    l

--- HackatonFront/src/app/MyHuella/huella/huella.page.ts
+++ HackatonFront/src/app/MyHuella/huella/huella.page.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Pyme } from '../../Entidades/Pyme';
 import {PrincipalServiceService} from '../../Servicios/principal-service.service';
 import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
 
 
 @Component({
@@ -15,9 +17,10 @@ export class HuellaPage implements OnInit {
 	idPyme:number;
 	myPyme:Pyme;
   CalculoHuella: number=0;
-  soluciones:Pyme[]=[];  //la primera es original
+  pymesSoluciones:Pyme[]=[];  //la primera es original
+  solucionActual:Pyme;
 
-  constructor(private route: ActivatedRoute,private principalService:PrincipalServiceService, private navCtrl: NavController) { }
+  constructor(private route: ActivatedRoute,private principalService:PrincipalServiceService, private navCtrl: NavController,private alertCtrl: AlertController) { }
 
   ngOnInit() {
   	this.route.params.subscribe(params => {
@@ -29,12 +32,53 @@ export class HuellaPage implements OnInit {
           console.log(this.myPyme);
           this.principalService.ObtenerHuellaSoluciones(this.myPyme)
           .subscribe((P:Pyme[])=>{
-             this.soluciones=P;
-             console.log("soluciones",this.soluciones);
+            this.solucionActual=P[0];
+            console.log("Solucion Actual",this.solucionActual);
+             for (var i = 1; i < P.length; i++) {
+               this.pymesSoluciones.push(P[i]);
+             }
+             console.log("soluciones",this.pymesSoluciones);
           });
       });
 
     });
   }
+
+
+  // alert de cada servicio
+  async sol(servicios) {
+    //console.log(servicio);
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: "Servicio de "+servicios.nombreServicio,
+      message : `<img src="assets/Imagenes/Servicios/Agua.png/>`,  
+      inputs: [
+      {
+        name: `<img src="assets/Imagenes/Servicios/Agua.png/>`,
+        placeholder: 'jeje'
+      }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (datos) => {
+            // Se agregan los datos leidos
+            
+            console.log(servicios);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+
 
 }
